a94459806e3ee4e895d9d50fa26cb9712a8118f0
Giovanni Perez <giovanniperez@Giovannis-MacBook-Pro.local>
2017-12-20 16:23:25 -0600

    sytntax practice

--- newbie.py
+++ newbie.py
@@ -1,1 +1,80 @@
+# apparently, a hashtag is a comment 
+# command question mark does the trick
+
 print('Hello World')
+
+my_message = 'Carry out my wayward son'
+
+print(my_message)
+
+kansas = """There'll be peace when you are done
+Lay your weary head to rest
+Don't you cry no more"""
+
+print(len(kansas))
+
+
+greeting = 'Hey'
+name = 'Giovanni'
+
+together = '{}, {}. Nice going!'.format(greeting, name)
+
+print(together)
+
+
+
+print(4 ** 4)
+print(5 * (3 + 9))
+
+num = 2
+
+num *= 10
+
+print(num)
+
+print(abs(-3))
+
+print(round(3.75, 1))
+
+
+# List, Tuple, & Set time!
+
+fruits = ['banana', 'apple', 'dragonfruit', 'tomato']
+
+print(fruits[2:])
+
+fruits.append('potato')
+
+print(fruits)
+
+fruits.insert(0, 'avocado')
+
+print(fruits)
+
+fruits.remove('potato')
+
+fruits.pop()
+
+print(fruits)
+
+fruits.sort()
+
+print(fruits)
+
+
+numeros = [2,1,6,9,4,5]
+
+numeros.sort(reverse=True)
+
+print(numeros)
+
+
+for item in fruits:
+	print(item)
+
+
+
+
+
+
+
